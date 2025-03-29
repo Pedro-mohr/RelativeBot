2752dd651e03e5d79c1a673a3115db1f5343b293
Pedro Mohr <159863379+Pedro-mohr@users.noreply.github.com>
2025-03-29 14:29:17 -0300

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,15 +1,16 @@
 from flask import Flask
 from threading import Thread
+import os
 
-
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "Bot is running!"
 
 def run():
-    app.run(host='0.0.0.0',port=8000)
+    port = int(os.environ.get("PORT", 8000))  # Usar variable PORT
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     server = Thread(target=run)
